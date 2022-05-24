6b776684c2d9855cbf30ca2139812ebe31a3528e
Gabriel RR <gabriel1998bb@gmail.com>
2022-05-23 23:05:18 -0300

    Tudo pronto :D
    TUDO FUNCIONADOOO

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function escalak () {
 function escalaf () {
 
   let temp = document.getElementById("temperatura")
-  temp.textContent = "fahrenheit"
+  temp.textContent = "Fahrenheit"
 
 }
 
@@ -37,18 +37,53 @@ function converterk () {
 function converterf () {
 
   let converter = document.getElementById("converter")
-  converter.textContent = "fahrenheit"
+  converter.textContent = "Fahrenheit"
 
 }
 
 function resolver () {
+  
   let resultado = document.getElementById("resultado")
   let converter = document.getElementById("converter")
   let temp = document.getElementById("temperatura")
   let numero = document.getElementById("numero")
-
+  let K = 273.15
+  let F = 32
+ 
   if(temp.textContent === "Celsius" && converter.textContent === "Celsius") {
-    console.log(numero.value)
+    resultado.innerHTML = numero.value
+  } 
+
+  if(temp.textContent === "Celsius" && converter.textContent === "Kelvin") {
+    resultado.innerHTML = parseInt(numero.value) + K + 'K'  
+  }
+
+  if(temp.textContent === "Kelvin" && converter.textContent === "Celsius") {
+    resultado.innerHTML = parseInt(numero.value) + K + 'K'  
+  }
+
+  if(temp.textContent === "Kelvin" && converter.textContent === "Kelvin") {
+    resultado.innerHTML = 273.15  
+  }
+
+  if(temp.textContent === "Celsius" && converter.textContent === "Fahrenheit") {
+    resultado.innerHTML = ( parseInt(numero.value) * 9/5) + 32 + 'F'
+  }
+
+  if(temp.textContent === "Fahrenheit" && converter.textContent === "Celsius") {
+    resultado.innerHTML = ( parseInt(numero.value) * 9/5) + 32 + 'F'
+  }
+  
+  if(temp.textContent === "Fahrenheit" && converter.textContent === "Fahrenheit") {
+    resultado.innerHTML =  32 + 'F'  
+  }
+  
+  if(temp.textContent === "Fahrenheit" && converter.textContent === "Kelvin") {
+    resultado.innerHTML =  (numero.value - 32) * 5/9 + K
+  }
+
+  if(temp.textContent === "Kelvin" && converter.textContent === "Fahrenheit") {
+    resultado.innerHTML =  (numero.value - 32) * 5/9 + K
   }
  
 
